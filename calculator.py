24a16ab8600fbc1efe195513ea2d2a38867b7b8a
Hirokun404 <96764524+Hirokun404@users.noreply.github.com>
2022-01-11 22:20:57 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,29 @@
+#calculator
+
+print("select the option\n1)add\n2)sub\n3)multiply\n4)divide")
+a=int(input())
+
+#for addition
+if (a==1):
+    x=int(input("enter first number to add"))
+    y=int(input("enter second number"))
+    print("addiction is",x+y)
+
+#for subtraction
+if (a==2):
+    x=int(input("enter first number to sub"))
+    y=int(input("enter second number"))
+    print("subtraction is : ",x-y)
+#for multiplication
+if (a==3):
+    x=int(input("enter first number to multiplication"))
+    y=int(input("enter second number"))
+    print("multiplication is : ",x*y)
+#for divison
+if (a==1):
+    x=int(input("enter first number to divison"))
+    y=int(input("enter second number"))
+    print("divison is : ",x/y)
+
+
+
